03031a10fcb0e05d70d019aadfc6853e5d76ea73
Thomas Sonderman <thomassonderman@Thomass-MacBook-Pro.local>
2017-02-19 08:08:12 -0500

    update

--- majority-voting/train_eval.py
+++ majority-voting/train_eval.py
@@ -99,19 +99,14 @@ for o in range(0,5):
     print("Negative Test Size{}".format(negative_test_size))
     print("Positive Train Size{}".format(positive_train_size))
     print("Negative Train Size{}".format(negative_train_size))
-
-
-    pos_cut_dev = list_positive_instances[int(positive_train_size):]
-    print(pos_cut_dev)
-    neg_cut_dev = list_negative_instances[int(negative_train_size):]
-
+    pos_cut_dev = list_positive_instances[-int(positive_test_size):]
+    neg_cut_dev = list_negative_instances[-int(negative_test_size):]
     positive_labels = [[0, 1] for _ in pos_cut_dev]
     negative_labels = [[1, 0] for _ in neg_cut_dev]
     print("Length of Cut positive test:%s", len(pos_cut_dev))
     print("Length of Cut negative test:%s", len(neg_cut_dev))
-    list_positive_instances = list_positive_instances[:int(positive_train_size)]
-    print(list_positive_instances)
-    list_negative_instances = list_negative_instances[:int(negative_train_size)]
+    list_positive_instances = list_positive_instances[:-int(positive_train_size)]
+    list_negative_instances = list_negative_instances[:-int(negative_train_size)]
     print("Length of Cut positive train:%s", len(list_positive_instances))
     print("Length of Cut negative train:%s", len(list_negative_instances))
     y_dev = np.concatenate([positive_labels, negative_labels], 0)
